c55d9a6acf7c2de1acb3072a3927d71eddf5cfed
Zeliha Nur UZTİMÜR <78546860+uztnur@users.noreply.github.com>
2022-08-22 23:26:00 +0300

    Ders -1

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿//DERS-1
+Console.Write("Merhaba Dünya");//Yazdırma fonksiyonudur.
+Console.Read();//Enter'a basılıncaya kadar Consol ekranını açık tutar.
+Console.WriteLine("Merhaba Nur ");//Sonraki ifadeyi alt satıra geçerek yazdırır.
+
